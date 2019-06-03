8dc72cf4e146f99fddf792d99fd006be655e7352
sl <dianzikezhang@163.com>
2019-06-03 13:54:52 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'blogitem',
         'USER': 'root',
-        'PASSWORD': '123456',#qwgxdcsltx3#
+        'PASSWORD': '#qwgxdcsltx3#',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
